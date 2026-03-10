7a32f0b9f637c812923f78a6d0191d1a21576cb5
The1stAtomic <your-email@gmail.com>
2026-03-10 23:33:42 +0700

    m request

--- src/utils/config/configs.js
+++ src/utils/config/configs.js
@@ -1,6 +1,6 @@
 
 
 export const configs = {
-    base_url : "http://127.0.0.1:8000/api/",
-    image_url :  "http://127.0.0.1:8000/storage/",
+    base_url: "https://ict-backend-c04v.onrender.com/api/",
+    image_url: "http://127.0.0.1:8000/storage/",
 }
