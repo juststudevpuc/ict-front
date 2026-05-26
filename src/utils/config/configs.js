f75e8c8349a8b2cadfb0e8deea2bbd5a7a5c4be5
The1stAtomic <your-email@gmail.com>
2026-05-26 11:52:42 +0700

    url /

--- src/utils/config/configs.js
+++ src/utils/config/configs.js
@@ -1,9 +1,10 @@
-
-
 export const configs = {
+    // Both point to your live Render server now!
     base_url: "https://ict-backend-c04v.onrender.com/api/",
-    image_url: "http://127.0.0.1:8000/storage/",
+    image_url: "https://ict-backend-c04v.onrender.com/storage/",
 }
+
+// Keep this commented out for when you want to test locally later
 // export const configs = {
 //     base_url: "http://127.0.0.1:8000/api/",
 //     image_url: "http://127.0.0.1:8000/storage/",
